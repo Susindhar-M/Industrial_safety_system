89618a18640d169d0de0b9d64df64511c0e758db
SUSINDHAR  M <70465430+Susindhar-M@users.noreply.github.com>
2025-07-12 19:53:44 +0200

    Update buzzer_led.py

--- Pi/actuators/buzzer_led.py
+++ Pi/actuators/buzzer_led.py
@@ -1,3 +1,10 @@
+######################################################################################################
+# Authors : Susindhar Manivasagan, Raksha Nagendra, Mansi Sharad Dongare
+######################################################################################################
+# Do not modify
+# actuator/buzzer_led.py
+
+# Importing libraries
 import grovepi
 import time
 
@@ -13,22 +20,20 @@ class BuzzerLED:
         for pin in self.led_pins.values():
             grovepi.pinMode(pin, "OUTPUT")
 
+    # LED and buzzer action for access granted
     def access_granted(self, zone="zone1"):
-        # Short beep + green LED
+        # Short beep + LED zone of particular zone
         grovepi.digitalWrite(self.buzzer_pin, 1)
         grovepi.digitalWrite(self.led_pins.get(zone, 5), 1)
         time.sleep(0.2)
         grovepi.digitalWrite(self.buzzer_pin, 0)
         grovepi.digitalWrite(self.led_pins.get(zone, 5), 0)
 
+    # LED and buzzer action for access denied
     def access_denied(self):
-        # Repeated buzz + blink both LEDs
+        # Repeated buzz 
         for _ in range(3):
-            grovepi.digitalWrite(self.buzzer_pin, 1)
-            for pin in self.led_pins.values():
-                grovepi.digitalWrite(pin, 1)
+            grovepi.digitalWrite(self.buzzer_pin, 1)            
             time.sleep(0.3)
-            grovepi.digitalWrite(self.buzzer_pin, 0)
-            for pin in self.led_pins.values():
-                grovepi.digitalWrite(pin, 0)
+            grovepi.digitalWrite(self.buzzer_pin, 0)            
             time.sleep(0.2)
